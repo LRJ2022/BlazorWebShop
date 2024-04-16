780eaff45c4912cb4ffd3db0b91bcfd318af1718
LRJ2022 <118718537+LRJ2022@users.noreply.github.com>
2024-04-16 10:51:17 +0200

    Add project files.

---
+++ BlazorApp1/Components/TableHeaderName.cs
@@ -0,0 +1,15 @@
+﻿namespace BlazorApp1.Components
+{
+    public class TableHeaderName
+    {
+        public TableHeaderName(string name, int colspan = 1)
+        {
+            Name = name;
+            Colspan = colspan;
+        }
+
+        public int Colspan { get; }
+
+        public string Name { get;}
+    }
+}
